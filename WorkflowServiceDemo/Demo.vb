d8a9bd298f1c57e2ff5cb5706a477e2e940fe542
Nghia Ngo <nghiant6554@co-well.com.vn>
2018-03-17 21:31:34 +0700

    Update

--- WorkflowServiceDemo/Demo.vb
+++ WorkflowServiceDemo/Demo.vb
@@ -7,8 +7,7 @@ Module Demo
 
     Sub Main()
         'Url của server API 
-        Dim url As String = "https://dev.e2-cloud.jp/workflow_stg/"
-        url = "https://dev.e2-cloud.jp/workflow/"
+        Dim url As String = "https://dev.e2-cloud.jp/workflow/"
         'client_id/client_secret, được lưu trong bảng oauth_client_details
         'client_id chính là company code
         Dim ws As New WorkflowService(url, "1111", "123456")
@@ -25,8 +24,8 @@ Module Demo
 
         ''Call create request
         ''Tạo request, bỏ cmt là chạy.
-        'create(ws)
 
+        Dim request As RequestInfo = create(ws)
         Dim requestInfo As Task(Of RequestInfo)
 
         ''Update
@@ -40,11 +39,15 @@ Module Demo
 
         'Apply
         'Apply request, trước khi apply cũng lấy thông tin request tương ứng
-        requestInfo = ws.GetRequest(4167)
-        If Not requestInfo.Result Is Nothing Then
-            'apply request, có sử dụng formdata
-            apply(ws, requestInfo.Result)
+        If Not request Is Nothing Then
+            requestInfo = ws.GetRequest(request.request.requestId)
+            requestInfo.Wait()
+            If Not requestInfo.Result Is Nothing Then
+                'apply request, có sử dụng formdata
+                apply(ws, requestInfo.Result)
+            End If
         End If
+
 
         'Delete
         ''Delete request với id truyền vào.
@@ -88,18 +91,6 @@ Module Demo
     Private Function create(ByRef ws As WorkflowService) As RequestInfo
         Dim requestCreation As New RequestCreation()
         'Set trị các tham số khi tạo request
-        requestCreation.requestFormDetailRelaId = "Form-E2move"
-        requestCreation.organizationRelaId = "Org-E2move"
-        requestCreation.amount = 1000
-        requestCreation.conditionNumber = 1
-        requestCreation.userName = "DuyenVTH"
-
-        'requestCreation.requestFormDetailRelaId = "e2move-detail"
-        'requestCreation.organizationRelaId = "E2-MOVE-ORG-2"
-        'requestCreation.amount = 1000
-        'requestCreation.conditionNumber = 1
-        'requestCreation.userName = "nghiant12345"
-
         requestCreation.requestFormDetailRelaId = "e2move"
         requestCreation.organizationRelaId = "組織連携123"
         requestCreation.amount = 1000
@@ -121,7 +112,7 @@ Module Demo
         Dim requestUpdate As New RequestUpdate()
         'Set trị các tham số khi update request
         requestUpdate.amount = 1000
-        requestUpdate.conditionNumber = 1
+        requestUpdate.conditionNumber = 0
 
         Dim updateRequest As Task(Of RequestInfo) = ws.UpdateRequest(request.request.requestId, requestUpdate)
         updateRequest.Wait()
@@ -143,7 +134,7 @@ Module Demo
         requestUpdate.amount = 1000
         'Form data được truyền lên theo format: [{"name":"control-name1","value":"value1"}, {"name":"control-name2","value":"value2"}]
         'Xử lý gán trị khi lấy thông tin control name từ formDesign
-        requestUpdate.formData = updateFormData(request.request.formDesign, request.request.requestId)
+        requestUpdate.formData = updateFormData(request.request.formDesign, request.request.requestId, ws)
         'requestUpdate.formData = "[{""type"":""paragraph"",""grid"":""col-sm-6"",""label"":""ラベル"",""subtype"":""p""},{""type"":""text"",""grid"":""col-sm-6"",""label"":""テキストフィールド"",""className"":""form-control"",""name"":""text-1521183155857"",""value"":""11111"",""subtype"":""text""},{""type"":""textarea"",""grid"":""col-sm-6"",""label"":""テキストエリア"",""className"":""form-control"",""name"":""textarea-1521183156448"",""value"":""22222""},{""type"":""html"",""grid"":""col-sm-6"",""label"":""HTML"",""name"":""html-1521183156992"",""html"":""<p>33333333</p>"",""height"":""372""},{""type"":""number"",""grid"":""col-sm-6"",""label"":""数値"",""className"":""form-control"",""name"":""number-1521183158022"",""value"":""444444""},{""type"":""caculator"",""grid"":""col-sm-6"",""label"":""電卓"",""name"":""caculator-1521183158386""},{""type"":""date"",""grid"":""col-sm-6"",""label"":""日付フィールド"",""className"":""form-control"",""name"":""date-1521183159962"",""value"":""2018/03/22""},{""type"":""monthyear"",""grid"":""col-sm-6"",""label"":""年月フィールド"",""name"":""monthyear-1521183160546"",""value"":""2018/05""},{""type"":""attach"",""grid"":""col-sm-6"",""label"":""添付ファイル"",""name"":""attach-1521183161106""},{""type"":""checkbox-group"",""grid"":""col-sm-6"",""label"":""チェックボックスグループ"",""name"":""checkbox-group-1521183162348"",""values"":[{""label"":""オプション 1"",""value"":""1"",""selected"":true}]},{""type"":""radio-group"",""grid"":""col-sm-6"",""label"":""ラジオ・グループ"",""name"":""radio-group-1521183163593"",""values"":[{""label"":""オプション 1"",""value"":""1"",""selected"":true},{""label"":""オプション 2"",""value"":""2""}]},{""type"":""select"",""grid"":""col-sm-6"",""label"":""選択"",""className"":""form-control"",""name"":""select-1521183164202"",""values"":[{""label"":""オプション 1"",""value"":""1""},{""label"":""オプション 2"",""value"":""2"",""selected"":true}],""selectmulti"":""2""},{""type"":""space"",""grid"":""col-sm-6"",""name"":""space-1521183164852""},{""type"":""hr"",""grid"":""col-sm-12"",""name"":""hr-1521183165444"",""hr"":""style1"",""hrheight"":""23""},{""type"":""header"",""grid"":""col-sm-6"",""label"":""ヘッダ"",""subtype"":""h1""},{""type"":""image"",""grid"":""col-sm-6"",""img"":""https://dev-workflow-1111.s3-ap-northeast-1.amazonaws.com/Attach%20File/4112-image-1521183167124.png"",""label"":""画像"",""name"":""image-1521183167124"",""value"":""C:\\fakepath\\build_result.png""}]"
         Dim temp As Staff
         'Set process
@@ -166,7 +157,7 @@ Module Demo
         Return Nothing
     End Function
 
-    Private Function updateFormData(ByVal formData As String, ByVal requestId As Long) As String
+    Private Function updateFormData(ByVal formData As String, ByVal requestId As Long, ByVal ws As WorkflowService) As String
         'Parse formDesign thành list, mỗi phần tử có name và value
         Dim formDataControls = JsonConvert.DeserializeObject(Of List(Of FormDataControl))(formData)
         For Each control As FormDataControl In formDataControls
@@ -174,8 +165,7 @@ Module Demo
             'Checkbox group, Radio group, Select control
             If control.type = "checkbox-group" Or control.type = "radio-group" Or control.type = "select" Then
                 control.values(0).selected = True
-                control.value = JsonConvert.SerializeObject(control.values)
-            ElseIf control.type = "text" Or control.type = "textarea" Then
+            ElseIf (control.type = "text" And control.name = "text-1521183155857") Or control.type = "textarea" Then
                 control.value = "test-value" + Rnd().ToString
             ElseIf control.type = "number" Then
                 control.value = Rnd().ToString
@@ -186,7 +176,11 @@ Module Demo
             ElseIf control.type = "html" Then
                 control.html = "<h1>Test</h1>"
             ElseIf control.type = "attach" Then
-                control.value = requestId.ToString() + "-" + control.name + "preview" + "-20180227_e2move連携API説明15.txt"
+                uploadFile(requestId, control.name + "-preview", "akimizu", "20180227_e2move連携API説明15.txt", "20180227_e2move連携API説明15.txt", ws)
+                control.attach = requestId.ToString() + "-" + control.name + "-preview" + "-20180227_e2move連携API説明15.txt"
+                control.attachname = "20180227_e2move連携API説明15.txt"
+            ElseIf control.type = "image" Then
+                control.img = "data:image/png;base64," + Convert.ToBase64String(File.ReadAllBytes("test.png"))
             ElseIf control.type = "paragraph" Or control.type = "header" Then
                 control.label = "test-value" + Rnd().ToString
             End If
